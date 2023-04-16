0346638089133a3c4904db2f6c34f4830beaa2ce
bibin-jaimon <bibinjaimon@gmail.com>
2023-04-16 22:00:08 +0530

    Refactor Fructus app rootview

--- Apps/Fructus/Fructus/Source/FARootView.swift
+++ Apps/Fructus/Fructus/Source/FARootView.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct FARootView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -19,8 +19,8 @@ struct ContentView: View {
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct FARootView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        FARootView()
     }
 }
