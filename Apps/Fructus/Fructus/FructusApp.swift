83529603a01efc91fad33a3da55433ebf067fe21
bibin-jaimon <bibinjaimon@gmail.com>
2023-04-16 21:19:27 +0530

    Fructus setup

---
+++ Apps/Fructus/Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Jaimon, Bibin on 16/04/23.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
